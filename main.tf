d730737754e444d8e43e06dd51dd9b9f87d015db
Nagi Reddy <nagireddychereddy630@gmail.com>
2024-12-05 22:04:16 +0000

    jenkins file

--- main.tf
+++ main.tf
@@ -67,6 +67,10 @@ resource "aws_route_table_association" "rta" {
   subnet_id      = aws_subnet.subnet1.id
   route_table_id = aws_route_table.rt.id
 }
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rt.id
+}
 # Associate the private route table with the private subnet (subnet3)
 resource "aws_route_table_association" "private_rta" {
   subnet_id      = aws_subnet.subnet3.id  # Assuming you rename the duplicate subnet1 to subnet3
