46f6054b45c6ec298fbc14ad9e275f6028c4863c
Nagi Reddy <nagireddychereddy630@gmail.com>
2024-12-05 19:49:05 +0000

    jenkins file

---
+++ main.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  region = "us-east-1" # Change to your desired region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "project-vpc"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a" # Change to your desired availability zone
+  tags = {
+    Name = "project-public-subnet-1"
+  }
+}
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b" # Change to your desired availability zone
+  tags = {
+    Name = "project-public-subnet-2"
+  }
+}
+resource "aws_subnet" "subnet3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1c" # Change to your desired availability zone
+  tags = {
+    Name = "project-private-subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "main-rt"
+  }
+}
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-rt"
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rt.id
+}
+# Associate the private route table with the private subnet (subnet3)
+resource "aws_route_table_association" "private_rta" {
+  subnet_id      = aws_subnet.subnet3.id  # Assuming you rename the duplicate subnet1 to subnet3
+  route_table_id = aws_route_table.private_rt.id
+}
